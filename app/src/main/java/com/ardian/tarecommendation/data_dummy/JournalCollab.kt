145cad9f765daf4794f16029e1096504a6b9a396
Muhammad Dani Arya Putra <13120210004@umi.ac.id>
2024-06-10 21:08:20 +0800

    refactor : pada journalRfy menambah data dummy. journal colllab adalah data dummy untuk journal collaborative

--- app/src/main/java/com/ardian/tarecommendation/data_dummy/JournalCollab.kt
+++ app/src/main/java/com/ardian/tarecommendation/data_dummy/JournalCollab.kt
@@ -4,94 +4,94 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class JournalRfy(
+data class JournalCollab(
     val title: String?,
     val author: String?,
     val published: String?,
     val keyword: ArrayList<String>?
 ) : Parcelable
 
-fun getJournal(): List<JournalRfy> {
+fun getJournalCollab(): List<JournalCollab> {
     return listOf(
-        JournalRfy(
+        JournalCollab(
             "Learning Analytics: Game-based Learning for Programming Course in Higher Education",
             "Priyaadharshini M, Natha Mayil N, R Dakshina, Sandhya S., Bettina Shirley R",
             "19 June 2020",
             (arrayListOf("Game Based Learning", "Learning", "Programming", "Higher Education", "Programming"))
         ),
-        JournalRfy(
+        JournalCollab(
             "Klasifikasi Gambar Menggunakan VGG16",
             "Arya, Furqon, Akbar",
             "17 Agustus 2021",
             (arrayListOf("Machine Learning", "Deep Learning", "Programming"))
         ),
-        JournalRfy(
+        JournalCollab(
             "Learning Analytics: Game-based Learning for Programming Course in Higher Education",
             "Priyaadharshini M, Natha Mayil N, R Dakshina, Sandhya S., Bettina Shirley R",
             "19 June 2020",
             (arrayListOf("Game Based Learning", "Learning", "Programming", "Higher Education", "Programming"))
         ),
-        JournalRfy(
+        JournalCollab(
             "Klasifikasi Gambar Menggunakan VGG16",
             "Arya, Furqon, Akbar",
             "17 Agustus 2021",
             (arrayListOf("Machine Learning", "Deep Learning", "Programming"))
         ),
-        JournalRfy(
+        JournalCollab(
             "Learning Analytics: Game-based Learning for Programming Course in Higher Education",
             "Priyaadharshini M, Natha Mayil N, R Dakshina, Sandhya S., Bettina Shirley R",
             "19 June 2020",
             (arrayListOf("Game Based Learning", "Learning", "Programming", "Higher Education", "Programming"))
         ),
-        JournalRfy(
+        JournalCollab(
             "Klasifikasi Gambar Menggunakan VGG16",
             "Arya, Furqon, Akbar",
             "17 Agustus 2021",
             (arrayListOf("Machine Learning", "Deep Learning", "Programming"))
         ),
-        JournalRfy(
+        JournalCollab(
             "Learning Analytics: Game-based Learning for Programming Course in Higher Education",
             "Priyaadharshini M, Natha Mayil N, R Dakshina, Sandhya S., Bettina Shirley R",
             "19 June 2020",
             (arrayListOf("Game Based Learning", "Learning", "Programming", "Higher Education", "Programming"))
         ),
-        JournalRfy(
+        JournalCollab(
             "Klasifikasi Gambar Menggunakan VGG16",
             "Arya, Furqon, Akbar",
             "17 Agustus 2021",
             (arrayListOf("Machine Learning", "Deep Learning", "Programming"))
         ),
-        JournalRfy(
+        JournalCollab(
             "Learning Analytics: Game-based Learning for Programming Course in Higher Education",
             "Priyaadharshini M, Natha Mayil N, R Dakshina, Sandhya S., Bettina Shirley R",
             "19 June 2020",
             (arrayListOf("Game Based Learning", "Learning", "Programming", "Higher Education", "Programming"))
         ),
-        JournalRfy(
+        JournalCollab(
             "Klasifikasi Gambar Menggunakan VGG16",
             "Arya, Furqon, Akbar",
             "17 Agustus 2021",
             (arrayListOf("Machine Learning", "Deep Learning", "Programming"))
         ),
-        JournalRfy(
+        JournalCollab(
             "Learning Analytics: Game-based Learning for Programming Course in Higher Education",
             "Priyaadharshini M, Natha Mayil N, R Dakshina, Sandhya S., Bettina Shirley R",
             "19 June 2020",
             (arrayListOf("Game Based Learning", "Learning", "Programming", "Higher Education", "Programming"))
         ),
-        JournalRfy(
+        JournalCollab(
             "Klasifikasi Gambar Menggunakan VGG16",
             "Arya, Furqon, Akbar",
             "17 Agustus 2021",
             (arrayListOf("Machine Learning", "Deep Learning", "Programming"))
         ),
-        JournalRfy(
+        JournalCollab(
             "Learning Analytics: Game-based Learning for Programming Course in Higher Education",
             "Priyaadharshini M, Natha Mayil N, R Dakshina, Sandhya S., Bettina Shirley R",
             "19 June 2020",
             (arrayListOf("Game Based Learning", "Learning", "Programming", "Higher Education", "Programming"))
         ),
-        JournalRfy(
+        JournalCollab(
             "Klasifikasi Gambar Menggunakan VGG16",
             "Arya, Furqon, Akbar",
             "17 Agustus 2021",
