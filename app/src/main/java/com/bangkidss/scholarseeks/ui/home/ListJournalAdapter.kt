fb0cba64fa12602a4efa485a8410064218f73b1e
Muhammad Dani Arya Putra <13120210004@umi.ac.id>
2024-06-18 23:04:10 +0800

    refactor : Data belum muncul di card. tapi response sudah berhasil

--- app/src/main/java/com/bangkidss/scholarseeks/ui/home/ListJournalAdapter.kt
+++ app/src/main/java/com/bangkidss/scholarseeks/ui/home/ListJournalAdapter.kt
@@ -11,13 +11,13 @@ import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bangkidss.scholarseeks.R
 import com.bangkidss.scholarseeks.api.RecomArticleResponseItem
+import com.bangkidss.scholarseeks.data_dummy.JournalRfy
 import com.bangkidss.scholarseeks.databinding.JournalCardBinding
 import com.bangkidss.scholarseeks.ui.detailJournal.DetailJournalActivity
 import com.google.android.flexbox.FlexboxLayout
 
 class ListJournalAdapter(private val listJournal: List<RecomArticleResponseItem>) :
     RecyclerView.Adapter<ListJournalAdapter.ListViewHolder>() {
-
     inner class ListViewHolder(val binding: JournalCardBinding) :
         RecyclerView.ViewHolder(binding.root)
 
@@ -36,30 +36,36 @@ class ListJournalAdapter(private val listJournal: List<RecomArticleResponseItem>
             tvYear.text = journal.year.toString()
 
             keywordContainer.removeAllViews()
-            journal.indexKeywords.split(", ").take(4).forEach { keyword -> // Assuming indexKeywords is a comma-separated string
-                val textView = TextView(root.context).apply {
-                    text = keyword
-                    layoutParams = FlexboxLayout.LayoutParams(
-                        FlexboxLayout.LayoutParams.WRAP_CONTENT,
-                        FlexboxLayout.LayoutParams.WRAP_CONTENT
-                    ).apply {
-                        setMargins(0, 12, 8, 12)
-                    }
+            journal.indexKeywords.forEach { keyword ->
+                keyword.let {
+                    val textView = TextView(root.context).apply {
+                        text = it.toString()
+                        layoutParams = FlexboxLayout.LayoutParams(
+                            FlexboxLayout.LayoutParams.WRAP_CONTENT,
+                            FlexboxLayout.LayoutParams.WRAP_CONTENT
+                        ).apply {
+                            setMargins(0, 12, 8, 12) // Equivalent to padding with top margin
+                        }
 
-                    setBackgroundResource(R.drawable.rounded_textview)
-                    setPadding(16, 8, 16, 8)
-                    typeface = ResourcesCompat.getFont(context, R.font.poppins_medium)
-                    setTextColor(ContextCompat.getColor(context, R.color.white))
-                    setTextSize(TypedValue.COMPLEX_UNIT_SP, 12f)
-                    textAlignment = View.TEXT_ALIGNMENT_CENTER
+                        setBackgroundResource(R.drawable.rounded_textview) // Background resource
+                        setPadding(16, 8, 16, 8) // Padding
+                        typeface = ResourcesCompat.getFont(context, R.font.poppins_medium) // Font
+                        setTextColor(ContextCompat.getColor(context, R.color.white)) // Text color
+                        setTextSize(TypedValue.COMPLEX_UNIT_SP, 8f) // Text size in SP
+                        textAlignment = View.TEXT_ALIGNMENT_CENTER // Text alignment
+                    }
+                    keywordContainer.addView(textView)
                 }
-                keywordContainer.addView(textView)
             }
         }
-
+        // melakukan perpindahan ke detail journal menggunakan intent
         holder.itemView.setOnClickListener {
             val intentDetail = Intent(holder.itemView.context, DetailJournalActivity::class.java)
-            intentDetail.putExtra(DetailJournalActivity.EXTRA_DETAIL, journal)
+            @Suppress("DEPRECATION")
+            intentDetail.putExtra(
+                DetailJournalActivity.EXTRA_DETAIL,
+                listJournal[holder.adapterPosition]
+            )
             holder.itemView.context.startActivity(intentDetail)
         }
     }
